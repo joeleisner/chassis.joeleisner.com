7f6a1d93aab6549bf7a66268f3c638a5cc08980a
Joel Eisner <jeisner93@gmail.com>
2019-08-08 20:49:41 -0400

    Updated the 404 page to have a summary included in the header of the layout as well as a more thorough 404 message

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,24 @@
 import React from 'react';
 
 import Layout from '../components/layout';
-import SEO from '../components/seo';
+import SEO    from '../components/seo';
+
+const Summary = () => (
+    <>
+        <h1>Page not found.</h1>
+        <p>Uh oh! The page you requested doesn't seem to exist.</p>
+    </>
+);
 
 const NotFoundPage = () => (
-    <Layout>
-        <SEO title="404: Not found" />
-        <h1>NOT FOUND</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Layout summary={ Summary }>
+        <SEO title="Page Not found" />
+        <h2>Things you can do.</h2>
+        <ul>
+            <li>Double check the URL for any misspellings.</li>
+            <li>Checkout the sitemap for pages that are similar to what you're looking for.</li>
+            <li>If you continue to reach this page, please contact <a href="mailto:jeisner93@gmail.com">jeisner93@gmail.com</a> so I can fix the mistake.</li>
+        </ul>
     </Layout>
 );
 
