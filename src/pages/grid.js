c62db729885dd7fa5b5d1dab69e9ee7c792b8509
Joel Eisner <jeisner93@gmail.com>
2019-08-10 17:01:57 -0400

    Updated the grid page to simplify the summary structure as well as fix any description of the extra small viewport size to be accurate

--- src/pages/grid.js
+++ src/pages/grid.js
@@ -10,12 +10,8 @@ const title     = 'Grid system',
 
 const Summary = () => (
     <>
-        <div className="header__summary">
-            <div>
-                <h1>{ title }</h1>
-                <p>{ description }</p>
-            </div>
-        </div>
+        <h1>{ title }</h1>
+        <p>{ description }</p>
     </>
 );
 
@@ -30,7 +26,7 @@ const IndexPage = () => (
             <li>Rows are meant to be inside a <code>.container</code>.</li>
             <li>Columns are meant to be inside a <code>.row</code>.</li>
             <li>Column widths can add up to 12 within a row.</li>
-            <li>Columns will become 100% width and vertically stacked when the viewport is less than or equal to 768px wide.</li>
+            <li>Columns will become 100% width and vertically stacked when the viewport is less than 768px wide.</li>
         </ul>
         <p>Makes sense, right? Let's dive deeper.</p>
         <h3>Fixed width columns</h3>
@@ -110,7 +106,7 @@ const IndexPage = () => (
 </div>`}</Code>
         <p>In the above example, the first column is aligned to the top of the row, the second column is aligned to the middle of the row, and the third column is aligned to the bottom of the row.</p>
         <Alert type="warning">
-            <p>Due to how flexbox works, vertical column alignments will become horizontal column alignments when the viewport is less than or equal to 768px.</p>
+            <p>Due to how flexbox works, vertical column alignments will become horizontal column alignments when the viewport is less than 768px.</p>
         </Alert>
         <h3>Horizontal column alignment</h3>
         <p>Columns can also be vertically aligned using the "justify content" modifier classes. These are added to the row and follow the <code>.jc-*</code> class structure, where <code>*</code> is one of the following letters:</p>
@@ -151,7 +147,7 @@ const IndexPage = () => (
 </div>`}</Code>
         <p>In the above example, each row has one of the possible "justify content" modifer classes on it.</p>
         <Alert type="warning">
-            <p>Due to how flexbox works, horizontal column alignments will become vertical column alignments when the viewport is less than or equal to 768px.</p>
+            <p>Due to how flexbox works, horizontal column alignments will become vertical column alignments when the viewport is less than 768px.</p>
         </Alert>
         <h2>Column offsets</h2>
         <p>Columns can be offset from the start of a row or from a column to the left of itself using the "offset" modifier classes. These are added to a column and follow the <code>.os-*</code> class structure, where <code>*</code> is one of the following:</p>
@@ -159,7 +155,7 @@ const IndexPage = () => (
             <li><code>a</code> means "auto" and puts as much space as possible to the left of the column.</li>
             <li>Any number between 1 and 12 corresponding to a column width.</li>
         </ul>
-        <p>When the viewport is less than or equal to 768px wide, these offsets will be removed. Let's see this in action:</p>
+        <p>When the viewport is less than 768px wide, these offsets will be removed. Let's see this in action:</p>
         <Code>{`<div class="container">
     <div class="row">
         <div class="col-4">.col-4</div>
@@ -176,7 +172,7 @@ const IndexPage = () => (
         <p>Columns can be ordered relatively from one another independently of the order they are placed in the row using the "order" modifier classes. These are added to a column and follow one of the following class structures where <code>*</code> is any number 1 and 12:</p>
         <ul>
             <li><code>.or-*</code> changes the order of a column on all size viewports.</li>
-            <li><code>.or-r*</code> changes the order of a column but resets the order when the viewport is less than or equal to 768px wide. This is similar behavior to the previous "push/pull" modifier classes in previous versions of this framework.</li>
+            <li><code>.or-r*</code> changes the order of a column but resets the order when the viewport is less than 768px wide. This is similar behavior to the previous "push/pull" modifier classes in previous versions of this framework.</li>
         </ul>
         <Code>{`<div class="container">
     <div class="row">
@@ -190,9 +186,9 @@ const IndexPage = () => (
         <div class="col-4 or-r1">.col-4.or-r1</div>
     </div>
 </div>`}</Code>
-        <p>In the above example, the first row has columns that are rendered in reverse order despite the order they were placed. The second row is doing the same as the first row, except this reording will be reset when the viewport is less than or equal to 768px wide.</p>
+        <p>In the above example, the first row has columns that are rendered in reverse order despite the order they were placed. The second row is doing the same as the first row, except this reording will be reset when the viewport is less than 768px wide.</p>
         <h3>Row reversing</h3>
-        <p>The first row from the above example can also be accomplished using the "reverse" modifier class. This is added to a row and uses the <code>.rev</code> class. This will not be reset when the viewport is less than or equal to 768px wide.</p>
+        <p>The first row from the above example can also be accomplished using the "reverse" modifier class. This is added to a row and uses the <code>.rev</code> class. This will not be reset when the viewport is less than 768px wide.</p>
         <Code>{`<div class="container">
     <div class="row rev">
         <div class="col-2">.col-2</div>
@@ -202,7 +198,7 @@ const IndexPage = () => (
 </div>`}</Code>
         <p>In the above example, the row has columns that are rendered in reverse order despite the order they were placed.</p>
         <Alert>
-            <p>If you're familiar with how CSS flexbox works, you can use the modifier classes listed above on any <code>display: flex</code> elements or its children, as these use the flexbox's alignment rules.</p>
+            <p>If you're familiar with how CSS flexbox works, you can use the modifier classes listed above on any <code>display: flex</code> elements or its children, as these use the flexbox alignment rules.</p>
         </Alert>
     </Layout>
 );
