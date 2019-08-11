7b3ae28c2b15adf7ec39ebfca49bef68866c661b
Joel Eisner <jeisner93@gmail.com>
2019-08-10 20:02:19 -0400

    Updated the external link React component to include always include a title and aria-label if one or the other is provided

--- src/components/externallink.js
+++ src/components/externallink.js
@@ -1,9 +1,13 @@
 import React     from 'react';
 import PropTypes from 'prop-types';
 
-const ExternalLink = ({ href, children, ...props }) => (
-    <a href={ href } rel="noopener noreferrer" target="_blank" { ...props }>{ children }</a>
-);
+const ExternalLink = ({ href, children, ...props }) => {
+    if (props.title && !props['aria-label']) props['aria-label'] = props.title;
+    if (!props.title && props['aria-label']) props.title         = props['aria-label'];
+    return (
+        <a href={ href } rel="noopener noreferrer" target="_blank" { ...props }>{ children }</a>
+    );
+};
 
 ExternalLink.propTypes = {
     href:     PropTypes.string.isRequired,
